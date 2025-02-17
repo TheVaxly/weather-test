45fdd805221a1cd6841a9f4572b5eae0eddc114d
Marten Uiboupin <marten.uiboupin@gmail.com>
2025-02-17 21:00:16 +0200

    Valitud linna ilmastiku info kuvamine

--- src/createMockServer.js
+++ src/createMockServer.js
@@ -1,6 +1,7 @@
 import { createServer } from 'miragejs';
 
 import searchResults from './search-results.json';
+import weather from './weather.json';
 
 const createMockServer = () => {
     return createServer({
@@ -8,6 +9,9 @@ const createMockServer = () => {
             this.urlPrefix = 'https://openweathermap.org';
             this.get('geo/1.0/direct', () => {
                 return searchResults;
+            });
+            this.get('data/2.5/onecall', () => {
+                return weather;
             });
         },
     });
